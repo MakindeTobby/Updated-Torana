e120703b195d1b77a9d64f18ca84917270cf9cc9
Juwurason <ajibolasunday7@gmail.com>
2023-02-11 02:45:31 +0100

    background_info added

--- src/pages/Background/BackgroundContent.js
+++ src/pages/Background/BackgroundContent.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 const BackgroundContent = () => {
     return (
         <>
-            <div className="doctor-details-area pt-100 pb-70">
+            <div className="doctor-details-area pt-100 pb-10">
+                
                 <div className="container">
                     <div className="row">
                         <div className="col-xl-7 col-lg-8">
@@ -21,46 +22,10 @@ const BackgroundContent = () => {
                                         <img src="img/shape/section-title-line.png" alt="" />
                                     </div>
                                 </div>
-                                <div className="service-details-text mb-25s">
-                                    <p>At Torana Care Australia, our staff work with us and not for us. Every staff is an
-                                        important member of our team and are given the opportunity to grow there career professionally.
-                                        We also support our workers to up skill with a view to achieving their career goals.</p>
-                                    <p>All our support and services are provided within Canberra. We currently have the following vacant positions:</p>
-
-
-
-
-
-                                    <div className="ser-fea-list fix">
-                                        <ul>
-                                            <li><i className='fas fa-check'></i>Disability Support Professional</li>
-                                            <li><i className='fas fa-check'></i> Registered Nurse</li>
-                                            <li><i className='fas fa-check'></i>Enrolled Nurse</li>
-                                            <li><i className='fas fa-check'></i>IT Coordinator</li>
-                                            <li><i className='fas fa-check'></i>Administration Officer</li>
-                                        </ul>
-                                    </div>
-                                    <p>
-                                        Get in touch with us.
-
-                                    </p>
-
-
-
-                                </div>
-
-                                <Link to="/contact" className="primary_btn btn-icon ml-0"
-                                    style={{ animationDelay: '0.6s' }} tabIndex="0"><span>+</span>Register your interest
-                                </Link>
-
 
 
                             </article>
                         </div>
-
-
-
-
 
                         <div className="col-xl-5 col-lg-4">
                             <div className="service-widget mb-50">
@@ -73,11 +38,104 @@ const BackgroundContent = () => {
 
                         </div>
                     </div>
+                </div>
 
 
+            </div>
+            <div className="doctor-details-area pt-10 pb-70">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-6 col-lg-6">
+                            <article className="doctor-details-box">
+                                <div className="section-title pos-rel mb-25">
+                                <h2>Florence</h2>
+                                <p>COFOUNDER</p>
+                            <div>
+                                <p>Florence has a Bachelors Degree in Electrical and Electronics Engineering(Telecommunication Engineering Option). She developed interest in banking and worked as an operation banker for  (four) 4 years before moving overseas. 
+                                She has overtime developed herself in different areas of human endeavors.</p>
+                            </div>
+                                </div>
+                            </article>
+                        </div>
 
+                        <div className="col-xl-6 col-lg-6">
+                        <article className="doctor-details-box">
+                                <div className="section-title pos-rel mb-25">
+                                <h2>Michael</h2>
+                                <p>COFOUNDER</p>
+                            <div>
+                                <p>Michael has a Bachelors of Medicine,Bachelor of Surgery (MBBS) and he is currently practicing 
+                                    as a Specialist General Practitioner (GP) here in the Australian Capital Territory.He worked overseas as a General 
+                                    Practitioner and Clinical Tutor for over (Fourteen years)14 years.</p>
+                            </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <p>
+                        Florence and Michael have always had profound Interest in providing support to vulnerable people within their community.  
+                        We currently provide financial support to children of low social economic levels, whom we are totally mindful of. Our profound dedication to this course was recognised and Florence was made a member of the board of Trustees of   <u>Child Plus Initiative.</u>
+                        </p>
+
+                        <p>
+                        Following our migration to Australia, we decided to provide more front-line service to vulnerable people, which informed Florence’s decision to get a job as a disability support worker back in 2015. Owing to her passion and interest in this area, she decided to study further in Individual Care and disability. Florence has actively worked with diverse participants for over four (4) years and has gained a wealth of experience, 
+                        caring for persons with complex and simple disabilities, in both community and residential home settings.
+                        </p>
+
+                        <p>
+                        We understand the needs of persons living with disabilities from both Medical, Social and Technological standpoints. We believe that, any human being is as strong as their “WILL” to achieve set goals, irrespective of their disability. On this principle, Torana Care Australia was born. Just like the saying, “where there is a will, there is a way”, we strengthen the “WILL” of our participants, to make “WAY” for them to be whatever they want to be, with our help. 
+                        Our Support and Services aim to bridge the gap between our participant’s “will” and the “way” to their goals.
+                        </p>
+                    </div>
+
+                    <div>
+                    <h4>Our Priority is to provide support and services that improve your quality of life...</h4>
+                    </div>
+
+                    <div className="mt-3">
+                    <div className="row">
+                        <div className="col-xl-6 col-lg-6">
+                            <article className="doctor-details-box">
+                                <div className="section-title pos-rel mb-25">
+                                <h4>Our Core Values</h4>
+                            <div>
+                                <p> <b>Involvement</b> because everybody deserves to be heard and be given a chance to be part of a team</p>
+                                <p> <b> Knowledge</b> because information and a habit of learning and general self development are the building blocks of excellent results</p>
+                                <p> <b> Determination</b> because perseverance is the key to success</p>
+                                <p> <b> Honesty</b> because it is the way to build trust and be true to oneself</p>
+                                <p> <b> Sensitive</b> because everyone is owed dignity & respect, irrespective of their language, culture, religion or beliefs.</p>
+                                <p> <b> Integrity</b> because having strong moral earns trust and influence</p>
+                                <p> <b> Fairness</b> because it brings the best out of everyone, which further enhances growth and success</p>
+                            </div>
+                                </div>
+                            </article>
+
+                        </div>
+
+                        <div className="col-xl-6 col-lg-6">
+                        <article className="doctor-details-box">
+                                <div className="section-title pos-rel mb-25">
+                                <h4>Our Principle</h4>
+                            <div>
+                            <p> The rights of persons living with a disability must be respected and supported in every way possible,in accordance with the law </p>
+                            <p> Unwavering act of respect to everyone including persons living with a disability</p>
+                            <p> Provide supports and services in a passionate,safe and competent manner</p>
+                            <p> Act with honesty, fairness and transparency at all times</p>
+                            <p> Proactively take steps to raise and act on concerns that might have an impact on the quality and safety of supports provided to persons with living with a disability</p>
+                            <p> Respond promptly to prevent and respond to all forms of violence, exploitation, neglect, and abuse of people with disability</p>
+                            <p> Persons living with a disability but give their consent at all times before delivering support and services</p>
+                            </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    </div>
 
                 </div>
+                
             </div>
         </>
     );
